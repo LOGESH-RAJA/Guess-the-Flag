b9b26cdb6ab8c99b114542db43dcbe8dde8e1099
Logesh R <logesh822081@gmail.com>
2024-11-29 09:57:22 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Logesh Raja on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
